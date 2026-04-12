a471e7796a2c46b884d64d059794229f466d6b0f
Hariravipati <harendraravipati9@gmail.com>
2026-04-12 20:08:40 +0530

    Code commit

--- src/router/lazyRoutes.tsx
+++ src/router/lazyRoutes.tsx
@@ -14,7 +14,7 @@ const createLazyComponent = (
 }
 
 export const Login = createLazyComponent(() => import('../feature-module/auth/login'))
-export const CardBoxCalculator = createLazyComponent(() => import('../feature-module/cardBoxCalculator/CardBoxCalculator'))
+export const CardBoxCalculator = createLazyComponent(() => import('../feature-module/CardBoxCalculator/CardBoxCalculator'))
 export const CreateArticle = createLazyComponent(() => import('../feature-module/createArticle/CreateArticle'))
 export const CreatePurchase = createLazyComponent(() => import('../feature-module/createPurchaseOrder/CreatePurchaseOrder'))
 export const Master = createLazyComponent(() => import('../feature-module/master/Master'))
